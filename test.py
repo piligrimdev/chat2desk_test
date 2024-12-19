9dcb84050ab6395f6558def7bcdc2fa74baeb324
Павел <78384513+piligrimdev@users.noreply.github.com>
2024-12-19 22:38:17 +0300

    Removed code repetition

--- test.py
+++ test.py
@@ -5,10 +5,64 @@ class Handler:
       "Authorization": f""
   }
 
-  def process_new_request(self, vip_tag_id: int, client_id: int, dialog_id: int) -> None:
-      client_data = self.get_client_by_id(client_id)
+  def _retrieve_until_meets_condition_(self, url: str, condition, **kwargs) -> object | None:
+      offset = 0
+      offset_step = 1
+      limit = 1
+      remaining = -1
 
-      if client_data and self.client_has_tag(client_data['data'], vip_tag_id):
+      params = {
+          'limit': limit,
+          'offset': offset
+      }
+
+      with requests.Session() as ses:
+          ses.headers = self.headers.copy()
+          while True:
+              params['limit'] = limit
+              params['offset'] = offset
+
+              response = ses.get(url, params=params)
+              response.raise_for_status()
+
+              resp_json = response.json()
+
+              result = condition(resp_json, **kwargs)
+              if result:
+                  return result
+
+              if remaining == -1:
+                  remaining = resp_json['meta']['total']
+
+              remaining -= limit
+              offset += offset_step
+
+              if remaining <= 0:
+                  return None
+
+  def available_operator_condition(self, resp_json: dict) -> int | None:
+      for operator in resp_json['data']:
+          if operator['opened_dialogs'] < 5:
+              return operator['id']
+      return None
+
+  def user_id_by_name_condition(self, resp_json: dict, username: str) -> int | None:
+      for client in resp_json['data']:
+          if client['username'] == username:
+              return client['id']
+      return None
+
+  def tag_id_by_label_condition(self, resp_json: dict, label: str) -> int | None:
+      for tag in resp_json['data']:
+          if tag['label'] == label:
+              return tag['id']
+      return None
+
+  def process_new_request(self, tag_label: str, client_id: int, dialog_id: int) -> None:
+      client_data = self.get_client_by_id(client_id)
+      tag_id = self.get_tag_id_by_label(tag_label)
+
+      if client_data and tag_id and self.client_has_tag(client_data['data'], tag_id):
           operator_id = self.get_available_operator()
           if operator_id:
               self.send_message_to_user(client_id, "Оператор найден", False, 'system')
@@ -19,10 +73,12 @@ class Handler:
   def process_external_post_request(self, username: str, tag_label: str) -> None:
       client_id = self.get_user_id_by_username(username)
 
-      if client_id != -1:
+      if client_id:
           self.send_message_to_user(client_id, f"Привет,{username}. Хорошего дня!", False, 'to_client')
+
           tag_id = self.get_tag_id_by_label(tag_label)
-          self.assign_tag_to_client(client_id, tag_id)
+          if tag_id:
+            self.assign_tag_to_client(client_id, tag_id)
 
   def client_has_tag(self, client_data: dict, tag_id: int) -> bool:
       for tag in client_data['tags']:
@@ -31,9 +87,6 @@ class Handler:
       return False
 
   def get_client_by_id(self, client_id: int) -> dict | None:
-      headers = {
-          "Authorization": f""
-      }
 
       response = requests.get(f'https://api.chat2desk.com/v1/clients/{client_id}', headers=self.headers)
 
@@ -43,11 +96,6 @@ class Handler:
       return None
 
   def set_operator_to_dialog(self, dialog_id: int, operator_id: int, state: str = None, initiator_id: int = None) -> int:
-      # https://api.chat2desk.com/v1/dialogs/18498876/
-
-      headers = {
-          "Authorization": f""
-      }
 
       if not state:
           state = 'closed'
@@ -61,15 +109,9 @@ class Handler:
       }
 
       response = requests.put(f' https://api.chat2desk.com/v1/dialogs/{dialog_id}', headers=self.headers, data=body)
-
-      return response.status_code
+      response.raise_for_status()
 
   def get_client_id_by_dialog_id(self, dialog_id: int) -> int | None:
-      # https://api.chat2desk.com/v1/dialogs/18498876/
-
-      headers = {
-          "Authorization": f""
-      }
 
       response = requests.get(f' https://api.chat2desk.com/v1/dialogs/{dialog_id}', headers=self.headers)
 
@@ -82,9 +124,6 @@ class Handler:
       return None
 
   def get_request_by_id(self, request_id: int) -> dict | None:
-      headers = {
-          "Authorization": f""
-      }
 
       response = requests.get(f'https://api.chat2desk.com/v1/requests/{request_id}', headers=self.headers)
 
@@ -94,48 +133,10 @@ class Handler:
       return None
 
   def get_available_operator(self) -> int | None:
-      # https://api.chat2desk.com/v1/operators/
+      return self._retrieve_until_meets_condition_('https://api.chat2desk.com/v1/operators/',
+                                                   self.available_operator_condition)
 
-      offset = 0
-      offset_step = 1
-      limit = 1
-      remaining = -1
-
-      headers = {
-          "Authorization": f""
-      }
-
-      while True:
-          params = {
-              'limit': limit,
-              'offset': offset
-          }
-
-          response = requests.get('https://api.chat2desk.com/v1/operators/', headers=self.headers,
-                                  params=params)
-
-          operator_json = response.json()
-
-          for operator in operator_json['data']:
-              if operator['opened_dialogs'] < 5:
-                  return operator['id']
-
-          if remaining == -1:
-              remaining = operator_json['meta']['total']
-
-          remaining -= limit
-          offset += offset_step
-
-          if remaining <= 0:
-              return None
-
-  def assign_tag_to_client(self, client_id: int, tag_id: int) -> int:
-      # https://api.chat2desk.com/v1/tags/assign_to
-
-      headers = {
-          "Authorization": f""
-      }
-
+  def assign_tag_to_client(self, client_id: int, tag_id: int) -> None:
       body = {
           'assignee_id': client_id,
           'tag_ids': [tag_id],
@@ -143,79 +144,19 @@ class Handler:
       }
 
       response = requests.post('https://api.chat2desk.com/v1/tags/assign_to', headers=self.headers, data=body)
+      response.raise_for_status()
 
-      return response.status_code
+  def get_user_id_by_username(self, username: str) -> int | None:
+      return self._retrieve_until_meets_condition_('https://api.chat2desk.com/v1/clients/',
+                                                   self.user_id_by_name_condition,
+                                                   username=username)
 
-  def get_user_id_by_username(self, username: str) -> int:
-        offset = 0
-        offset_step = 1
-        limit = 1
-        remaining = -1
-
-        while True:
-            params = {
-                'limit': limit,
-                'offset': offset
-            }
-
-            response = requests.get('https://api.chat2desk.com/v1/clients/', headers=self.headers,
-                                    params=params)
-
-            clients_json = response.json()
-
-            for client in clients_json['data']:
-                if client['username'] == username:
-                    return client['id']
-
-            if remaining == -1:
-                remaining = clients_json['meta']['total']
-
-            remaining -= limit
-            offset += offset_step
-
-            if remaining <= 0:
-                return -1
-
-  def get_tag_id_by_label(self, label: str) -> int:
-      offset = 0
-      offset_step = 1
-      limit = 1
-      remaining = -1
-
-      headers = {
-          "Authorization": f""
-      }
-
-      while True:
-          params = {
-              'limit': limit,
-              'offset': offset
-          }
-
-          response = requests.get('https://api.chat2desk.com/v1/tags/', headers=self.headers,
-                                  params=params)
-
-          tags_json = response.json()
-
-          for tag in tags_json['data']:
-              if tag['label'] == label:
-                  return tag['id']
-
-          if remaining == -1:
-              remaining = tags_json['meta']['total']
-
-          remaining -= limit
-          offset += offset_step
-
-          if remaining <= 0:
-              return -1
+  def get_tag_id_by_label(self, label: str) -> int | None:
+      return self._retrieve_until_meets_condition_('https://api.chat2desk.com/v1/tags/',
+                                                   self.tag_id_by_label_condition,
+                                                   label=label)
 
   def send_message_to_user(self, user_id: int, text: str, open_dialog: bool, type: str) -> int:
-      # https://api.chat2desk.com/v1/messages
-
-      headers = {
-          "Authorization": f""
-      }
 
       params = {
           'client_id': user_id,
@@ -226,16 +167,28 @@ class Handler:
 
       response = requests.post('https://api.chat2desk.com/v1/messages', headers=self.headers,
                                params=params)
-
-      return response.status_code
+      response.raise_for_status()
 
   def manually_handler(self, input_data, c2d):
     self.headers['Authorization'] = c2d.token
-    self.process_external_post_request(input_data['name'], 'VIP')
+
+    try:
+        self.process_external_post_request(input_data['name'], 'VIP')
+    except requests.exceptions.Timeout:
+        print('Request timed out')
+    except requests.exceptions.RequestException as e:
+        print('Exception raised by request method')
+
     return input_data['name']
 
   def new_request_handler(self, input_data, c2d):
     self.headers['Authorization'] = c2d.token
-    vip_id = self.get_tag_id_by_label('VIP')
-    self.process_new_request(vip_id, input_data['client_id'], input_data['dialog_id'])
+
+    try:
+        self.process_new_request('VIP', input_data['client_id'], input_data['dialog_id'])
+    except requests.exceptions.Timeout:
+        print('Request timed out')
+    except requests.exceptions.RequestException as e:
+        print(f'Exception raised by request method: {e}')
+
     return input_data
